b5448eae4c789aeac3e85454a915522d0e6a4e76
dnloop <29187104+dnloop@users.noreply.github.com>
2019-02-28 17:01:12 -0300

    redundant flush().

--- src/main/java/dao/PacientesHome.java
+++ src/main/java/dao/PacientesHome.java
@@ -44,7 +44,6 @@ public class PacientesHome {
             log.error(marker, "persist failed", re);
             throw re;
         } finally {
-            session.flush();
             session.close();
         }
     }
@@ -67,7 +66,6 @@ public class PacientesHome {
             log.debug(marker, "retrieve failed", re);
             throw re;
         } finally {
-            session.flush();
             session.close();
         }
         return list;
@@ -100,7 +98,6 @@ public class PacientesHome {
             log.error("update failed", re);
             throw re;
         } finally {
-            session.flush();
             session.close();
         }
     }
@@ -134,7 +131,6 @@ public class PacientesHome {
             log.error("delete failed", re);
             throw re;
         } finally {
-            session.flush();
             session.close();
         }
     }
