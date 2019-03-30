6531f9285894af8deb6e9270249553cc131047fb
dnloop <29187104+dnloop@users.noreply.github.com>
2019-03-30 15:36:13 -0300

    This new structure enables view switching.

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -1,39 +1,64 @@
 package main;
 
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 import org.apache.logging.log4j.core.Logger;
 
+import controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import utils.ViewSwitcher;
 
-public class MainApp extends Application{
+public class MainApp extends Application {
 
     protected static final Logger log = (Logger) LogManager.getLogger(MainApp.class);
     protected static final Marker marker = MarkerManager.getMarker("CLASS");
+
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
+    @Override
     public void start(Stage stage) throws Exception {
-
         log.info("[ Starting Sysvet application ]");
-
-        String fxmlFile = "/fxml/main.fxml";
-        log.debug(marker, "Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-        log.debug(marker, "Showing JFX scene");
-        setUserAgentStylesheet(STYLESHEET_CASPIAN);
-        Scene scene = new Scene(rootNode, 800, 420);
         stage.setTitle(" -·=[ SysVet ]=·-");
-        stage.setScene(scene);
-        //        stage.setResizable(false);
+        stage.setScene(createScene(loadMainPane()));
         stage.show();
+    }
+
+    /**
+     * Loads the main fxml layout. Sets up the vista switching VistaNavigator. Loads
+     * the first vista into the fxml layout.
+     *
+     * @return the loaded pane.
+     * @throws IOException if the pane could not be loaded.
+     */
+    private Pane loadMainPane() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        Pane mainPane = (Pane) loader.load(getClass().getResourceAsStream(ViewSwitcher.MAIN));
+        MainController mainController = loader.getController();
+        ViewSwitcher.setMainController(mainController);
+
+        return mainPane;
+    }
+
+    /**
+     * Creates the main application scene.
+     *
+     * @param mainPane the main application layout.
+     *
+     * @return the created scene.
+     */
+    private Scene createScene(Pane mainPane) {
+        Scene scene = new Scene(mainPane, 800, 420);
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+
+        return scene;
     }
 }
