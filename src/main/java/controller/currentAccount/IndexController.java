7f15fdb771d85ba3655a1aa692f499d355b8fe4b
dnloop <29187104+dnloop@users.noreply.github.com>
2019-04-04 12:22:12 -0300

    dialog not displaying correctly (linux apparently).

--- src/main/java/controller/currentAccount/IndexController.java
+++ src/main/java/controller/currentAccount/IndexController.java
@@ -174,6 +174,7 @@ public class IndexController {
         alert.setTitle("Confirmación");
         alert.setHeaderText("Confirmar acción.");
         alert.setContentText("¿Desea eliminar el registro?");
+        alert.setResizable(true);
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
@@ -189,6 +190,7 @@ public class IndexController {
         alert.setTitle("Advertencia.");
         alert.setHeaderText("Elemento vacío.");
         alert.setContentText("No se seleccionó ningún elemento de la lista. Elija un ítem e intente nuevamente.");
+        alert.setResizable(true);
 
         alert.showAndWait();
     }
