08b96296dd3dc11562e6aa62679a43e579b1d2ef
dnloop <29187104+dnloop@users.noreply.github.com>
2019-04-08 17:53:50 -0300

    created view with controllers: Clinical File.

--- src/main/java/controller/clinicalFile/IndexController.java
+++ src/main/java/controller/clinicalFile/IndexController.java
@@ -1,9 +1,279 @@
 package controller.clinicalFile;
 
-public class IndexController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
-    public IndexController() {
-        // TODO Auto-generated constructor stub
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+
+import dao.FichasClinicasHome;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import model.FichasClinicas;
+import utils.ViewSwitcher;
+
+public class IndexController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField txtFilter;
+
+    @FXML
+    private JFXButton btnNew;
+
+    @FXML
+    private JFXButton btnEdit;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXTreeTableView<FichasClinicas> indexCF;
+
+    protected static final Logger log = (Logger) LogManager.getLogger(IndexController.class);
+
+    private static FichasClinicasHome daoFC = new FichasClinicasHome();
+
+    private FichasClinicas fichaClinica;
+
+    private Integer id;
+
+    @SuppressWarnings("unchecked")
+    @FXML
+    void initialize() {
+        assert txtFilter != null : "fx:id=\"txtFilter\" was not injected: check your FXML file 'index.fxml'.";
+        assert btnNew != null : "fx:id=\"btnNew\" was not injected: check your FXML file 'index.fxml'.";
+        assert btnEdit != null : "fx:id=\"btnEdit\" was not injected: check your FXML file 'index.fxml'.";
+        assert btnDelete != null : "fx:id=\"btnDelete\" was not injected: check your FXML file 'index.fxml'.";
+        assert indexCF != null : "fx:id=\"indexCF\" was not injected: check your FXML file 'index.fxml'.";
+
+        JFXTreeTableColumn<FichasClinicas, String> paciente = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Paciente");
+        paciente.setPrefWidth(200);
+        paciente.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getPacientes())));
+
+        JFXTreeTableColumn<FichasClinicas, String> motivoConsulta = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Motivo Consulta");
+        motivoConsulta.setPrefWidth(200);
+        motivoConsulta.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getMotivoConsulta())));
+
+        JFXTreeTableColumn<FichasClinicas, String> anamnesis = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Anamnesis");
+        anamnesis.setPrefWidth(200);
+        anamnesis.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getAnamnesis())));
+
+        JFXTreeTableColumn<FichasClinicas, String> medicacionActual = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Medicación Actual");
+        medicacionActual.setPrefWidth(200);
+        medicacionActual.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getMedicacionActual())));
+
+        JFXTreeTableColumn<FichasClinicas, String> medicacionAnterior = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Medicación Anterior");
+        medicacionAnterior.setPrefWidth(200);
+        medicacionAnterior.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getMedicacionAnterior())));
+
+        JFXTreeTableColumn<FichasClinicas, String> estadoNutricion = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Estado Nutrición");
+        estadoNutricion.setPrefWidth(200);
+        estadoNutricion.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getEstadoNutricion())));
+
+        JFXTreeTableColumn<FichasClinicas, String> estadoSanitario = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Estado Sanitario");
+        estadoSanitario.setPrefWidth(200);
+        estadoSanitario.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getEstadoSanitario())));
+
+        JFXTreeTableColumn<FichasClinicas, String> aspectoGeneral = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Aspecto General");
+        aspectoGeneral.setPrefWidth(200);
+        aspectoGeneral.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getAspectoGeneral())));
+
+        JFXTreeTableColumn<FichasClinicas, String> deterDiagComp = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Determinaciones Diagnósticas Complementarias");
+        deterDiagComp.setPrefWidth(200);
+        deterDiagComp.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getDeterDiagComp())));
+
+        JFXTreeTableColumn<FichasClinicas, String> derivaciones = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Derivaciones");
+        derivaciones.setPrefWidth(200);
+        derivaciones.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getDerivaciones())));
+
+        JFXTreeTableColumn<FichasClinicas, String> pronostico = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Pronóstico");
+        pronostico.setPrefWidth(200);
+        pronostico.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getPronostico())));
+
+        JFXTreeTableColumn<FichasClinicas, String> diagnostico = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Diagnóstico");
+        diagnostico.setPrefWidth(200);
+        diagnostico.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getDiagnostico())));
+
+        JFXTreeTableColumn<FichasClinicas, String> exploracion = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Exploración");
+        exploracion.setPrefWidth(200);
+        exploracion.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getExploracion())));
+
+        JFXTreeTableColumn<FichasClinicas, String> evolucion = new JFXTreeTableColumn<FichasClinicas, String>(
+                "Evolución");
+        evolucion.setPrefWidth(200);
+        evolucion.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<FichasClinicas, String> param) -> new ReadOnlyStringWrapper(
+                        String.valueOf(param.getValue().getValue().getEvolucion())));
+
+        log.info("loading table items");
+        ObservableList<FichasClinicas> fichasList = FXCollections.observableArrayList();
+        fichasList = loadTable(fichasList);
+        TreeItem<FichasClinicas> root = new RecursiveTreeItem<FichasClinicas>(fichasList,
+                RecursiveTreeObject::getChildren);
+
+        indexCF.getColumns().setAll(paciente, motivoConsulta, anamnesis, medicacionActual, medicacionAnterior,
+                estadoNutricion, estadoSanitario, aspectoGeneral, deterDiagComp, derivaciones, pronostico, exploracion,
+                diagnostico, evolucion);
+        indexCF.setShowRoot(false);
+        indexCF.setRoot(root);
+
+        // Handle ListView selection changes.
+        indexCF.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            id = newValue.getValue().getId();
+            log.info("Item selected.");
+        });
+
+        btnEdit.setOnAction((event) -> {
+            if (id != null)
+                displayModal(event);
+            else
+                displayWarning();
+        });
+
+        btnDelete.setOnAction((event) -> {
+            if (id != null)
+                confirmDialog();
+            else
+                displayWarning();
+        });
     }
 
+    /**
+     *
+     * Class Methods
+     *
+     */
+
+    public void setView(String fxml) {
+        ViewSwitcher.loadView(fxml);
+    }
+
+    static ObservableList<FichasClinicas> loadTable(ObservableList<FichasClinicas> fichasClinicas) {
+
+        List<FichasClinicas> list = daoFC.displayRecords();
+        for (FichasClinicas ficha : list)
+            fichasClinicas.add(ficha);
+
+        return fichasClinicas;
+    }
+
+    private void confirmDialog() {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación");
+        alert.setHeaderText("Confirmar acción.");
+        alert.setContentText("¿Desea eliminar el registro?");
+        alert.setResizable(true);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            daoFC.delete(id);
+            indexCF.getSelectionModel().getSelectedItem().getParent().getChildren().remove(id - 1);
+            indexCF.refresh();
+            log.info("Item deleted.");
+        }
+    }
+
+    private void displayModal(Event event) {
+        Parent rootNode;
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/clinicalFile/modalDialog.fxml"));
+        Window node = ((Node) event.getSource()).getScene().getWindow();
+        fichaClinica = daoFC.showById(id);
+        try {
+            rootNode = (Parent) fxmlLoader.load();
+            ModalDialogController sc = fxmlLoader.getController();
+            sc.setObject(fichaClinica);
+            log.info("Loaded Item.");
+            stage.setScene(new Scene(rootNode));
+            stage.setTitle("Ficha Clinica - " + fichaClinica.getPacientes());
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(node);
+            stage.setOnHidden((stageEvent) -> {
+                indexCF.refresh();
+            });
+            sc.showModal(stage);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayWarning() {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle("Advertencia.");
+        alert.setHeaderText("Elemento vacío.");
+        alert.setContentText("No se seleccionó ningún elemento de la lista. Elija un ítem e intente nuevamente.");
+        alert.setResizable(true);
+
+        alert.showAndWait();
+    }
 }
--- src/main/java/controller/clinicalFile/ModalDialogController.java
+++ src/main/java/controller/clinicalFile/ModalDialogController.java
@@ -1,9 +1,192 @@
 package controller.clinicalFile;
 
-public class ModalDialogController {
+import java.net.URL;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
-    public ModalDialogController() {
-        // TODO Auto-generated constructor stub
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+
+import dao.FichasClinicasHome;
+import dao.PacientesHome;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import model.FichasClinicas;
+import model.Pacientes;
+
+public class ModalDialogController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXButton btnAccept;
+
+    @FXML
+    private JFXButton btnCancel;
+
+    @FXML
+    private JFXComboBox<?> comboPA;
+
+    @FXML
+    private JFXTextField txtMotivoConsulta;
+
+    @FXML
+    private JFXTextArea txtAnamnesis;
+
+    @FXML
+    private JFXTextField txtMedActual;
+
+    @FXML
+    private JFXTextField txtMedAnterior;
+
+    @FXML
+    private JFXTextField txtEstNutricion;
+
+    @FXML
+    private JFXTextField txtEstSanitario;
+
+    @FXML
+    private JFXTextField txtAspectoGeneral;
+
+    @FXML
+    private JFXTextField txtDerivaciones;
+
+    @FXML
+    private JFXTextArea txtDeterDiagComp;
+
+    @FXML
+    private JFXTextArea txtPronostico;
+
+    @FXML
+    private JFXTextArea txtDiagnostico;
+
+    @FXML
+    private JFXTextArea txtExploracion;
+
+    @FXML
+    private JFXTextArea txtEvolucion;
+
+    protected static final Logger log = (Logger) LogManager.getLogger(ModalDialogController.class);
+
+    static FichasClinicasHome daoFC = new FichasClinicasHome();
+
+    static PacientesHome daoPA = new PacientesHome();
+
+    private FichasClinicas fichaClinica;
+
+    private Stage stage;
+
+    @FXML
+    void initialize() {
+        assert btnAccept != null : "fx:id=\"btnAccept\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert comboPA != null : "fx:id=\"comboPA\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtMotivoConsulta != null : "fx:id=\"txtMotivoConsulta\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtAnamnesis != null : "fx:id=\"txtAnamnesis\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtMedActual != null : "fx:id=\"txtMedActual\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtMedAnterior != null : "fx:id=\"txtMedAnterior\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtEstNutricion != null : "fx:id=\"txtEstNutricion\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtEstSanitario != null : "fx:id=\"txtEstSanitario\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtAspectoGeneral != null : "fx:id=\"txtAspectoGeneral\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtDerivaciones != null : "fx:id=\"txtDerivaciones\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtDeterDiagComp != null : "fx:id=\"txtDeterDiagComp\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtPronostico != null : "fx:id=\"txtPronostico\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtDiagnostico != null : "fx:id=\"txtDiagnostico\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtExploracion != null : "fx:id=\"txtExploracion\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        assert txtEvolucion != null : "fx:id=\"txtEvolucion\" was not injected: check your FXML file 'modalDialog.fxml'.";
+        Platform.runLater(() -> {
+            log.info("Retrieving details");
+            // create list and fill it with dao
+            ObservableList<Pacientes> pacientes = FXCollections.observableArrayList();
+            pacientes = loadTable(pacientes);
+            // sort list elements asc by id
+            Comparator<Pacientes> comp = Comparator.comparingInt(Pacientes::getId);
+            FXCollections.sort(pacientes, comp);
+
+            log.info("Loading fields");
+            comboPA.getSelectionModel().select(fichaClinica.getPacientes().getId() - 1);
+            txtMotivoConsulta.setText(fichaClinica.getMotivoConsulta());
+            txtAnamnesis.setText(fichaClinica.getAnamnesis());
+            txtMedActual.setText(fichaClinica.getMedicacionActual());
+            txtMedAnterior.setText(fichaClinica.getMedicacionAnterior());
+            txtEstNutricion.setText(fichaClinica.getEstadoNutricion());
+            txtEstSanitario.setText(fichaClinica.getEstadoSanitario());
+            txtAspectoGeneral.setText(fichaClinica.getAspectoGeneral());
+            txtDerivaciones.setText(fichaClinica.getDerivaciones());
+            txtDeterDiagComp.setText(fichaClinica.getDeterDiagComp());
+            txtPronostico.setText(fichaClinica.getPronostico());
+            txtExploracion.setText(fichaClinica.getExploracion());
+            txtEvolucion.setText(fichaClinica.getEvolucion());
+        }); // required to prevent NullPointer
+
+        btnCancel.setOnAction((event) -> {
+            this.stage.close();
+        });
+
+        btnAccept.setOnAction((event) -> {
+            if (confirmDialog())
+                updateRecord();
+        });
     }
 
+    /**
+     *
+     * Class Methods
+     *
+     */
+
+    private void updateRecord() {
+        Date fecha = new Date();
+        fichaClinica.setUpdatedAt(fecha);
+        daoFC.update(fichaClinica);
+        log.info("record updated");
+        this.stage.close();
+    }
+
+    private boolean confirmDialog() {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación");
+        alert.setHeaderText("Confirmar acción.");
+        alert.setContentText("¿Desea actualizar el registro?");
+        alert.setResizable(true);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK)
+            return true;
+        else
+            return false;
+    }
+
+    static ObservableList<Pacientes> loadTable(ObservableList<Pacientes> paciente) {
+        List<Pacientes> list = daoPA.displayRecords();
+        for (Pacientes item : list)
+            paciente.add(item);
+        return paciente;
+    }
+
+    public void setObject(FichasClinicas fichaClinica) {
+        this.fichaClinica = fichaClinica;
+    }
+
+    public void showModal(Stage stage) {
+        this.stage = stage;
+        this.stage.showAndWait();
+    }
 }
