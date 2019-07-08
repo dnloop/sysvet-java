a89cc6f32895271df7437402325b937e0930d6cc
dnloop <29187104+dnloop@users.noreply.github.com>
2019-07-08 11:19:32 -0300

    loaded fields.

--- src/main/java/controller/clinicalFile/ViewController.java
+++ src/main/java/controller/clinicalFile/ViewController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import model.FichasClinicas;
 
@@ -28,9 +29,6 @@ public class ViewController {
 
     @FXML
     private JFXTextField txtMed;
-
-    @FXML
-    private JFXTextField txtMedAnterior;
 
     @FXML
     private JFXTextField txtEstNutricion;
@@ -67,7 +65,6 @@ public class ViewController {
         assert txtMotivoConsulta != null : "fx:id=\"txtMotivoConsulta\" was not injected: check your FXML file 'view.fxml'.";
         assert txtAnamnesis != null : "fx:id=\"txtAnamnesis\" was not injected: check your FXML file 'view.fxml'.";
         assert txtMed != null : "fx:id=\"txtMed\" was not injected: check your FXML file 'view.fxml'.";
-        assert txtMedAnterior != null : "fx:id=\"txtMedAnterior\" was not injected: check your FXML file 'view.fxml'.";
         assert txtEstNutricion != null : "fx:id=\"txtEstNutricion\" was not injected: check your FXML file 'view.fxml'.";
         assert txtEstSanitario != null : "fx:id=\"txtEstSanitario\" was not injected: check your FXML file 'view.fxml'.";
         assert txtAspectoGeneral != null : "fx:id=\"txtAspectoGeneral\" was not injected: check your FXML file 'view.fxml'.";
@@ -78,6 +75,20 @@ public class ViewController {
         assert txtExploracion != null : "fx:id=\"txtExploracion\" was not injected: check your FXML file 'view.fxml'.";
         assert txtEvolucion != null : "fx:id=\"txtEvolucion\" was not injected: check your FXML file 'view.fxml'.";
 
+        Platform.runLater(() -> {
+            txtFecha.setText(ficha.getFecha().toString());
+            txtAnamnesis.setText(ficha.getAnamnesis());
+            txtAspectoGeneral.setText(ficha.getAspectoGeneral());
+            txtDerivaciones.setText(ficha.getDerivaciones());
+            txtEstNutricion.setText(ficha.getEstadoNutricion());
+            txtDiagnostico.setText(ficha.getDiagnostico());
+            txtEstSanitario.setText(ficha.getEstadoSanitario());
+            txtEvolucion.setText(ficha.getEvolucion());
+            txtExploracion.setText(ficha.getExploracion());
+            txtMed.setText(ficha.getMedicacion());
+            txtMotivoConsulta.setText(ficha.getMotivoConsulta());
+            txtPronostico.setText(ficha.getPronostico());
+        });
     }
 
     public void setObject(FichasClinicas ficha) {
