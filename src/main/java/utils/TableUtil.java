f4ffe1c721adc93ff43b0495f7fb9f1ae9549913
dnloop <29187104+dnloop@users.noreply.github.com>
2019-07-09 20:00:21 -0300

    migration from java 8.

--- src/main/java/utils/TableUtil.java
+++ src/main/java/utils/TableUtil.java
@@ -1,23 +1,19 @@
 package utils;
 
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
-import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Pagination;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 public class TableUtil {
 
-    public static <T extends RecursiveTreeObject<T>> Node createPage(JFXTreeTableView<T> tableView,
-            ObservableList<T> data, Pagination pagination, Integer pageIndex, Integer rowSize) {
+    public static <T> Node createPage(TableView<T> tableView, ObservableList<T> data, Pagination pagination,
+            Integer pageIndex, Integer rowSize) {
         Integer fromIndex = pageIndex * rowSize;
         Integer toIndex = Math.min(fromIndex + rowSize, data.size());
-        tableView.setRoot(new RecursiveTreeItem<T>(FXCollections.observableArrayList(data.subList(fromIndex, toIndex)),
-                RecursiveTreeObject::getChildren));
+        tableView.setItems(FXCollections.observableArrayList(data.subList(fromIndex, toIndex)));
         pagination.setPageCount((data.size() / rowSize + 1));
 
         return new BorderPane();
