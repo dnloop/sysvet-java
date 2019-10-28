ec2922bd49e6af235b441ea4f848fcd9cb3cc368
dnloop <29187104+dnloop@users.noreply.github.com>
2019-10-28 09:43:26 -0300

    added validator and view helpers.

---
+++ src/main/java/utils/View.java
@@ -0,0 +1,19 @@
+package utils;
+
+/**
+ * @author dnloop
+ *
+ */
+
+public interface View {
+
+    public String indexView();
+
+    public String modalView();
+
+    public String newView();
+
+    public String showView();
+
+    public String recoverView();
+}
