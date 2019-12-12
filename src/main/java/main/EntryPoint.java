022106da888083be99cc784f37371d60f19831d9
dnloop <29187104+dnloop@users.noreply.github.com>
2019-12-12 17:19:58 -0300

    added maven shade plugin for javafx JAR package.

---
+++ src/main/java/main/EntryPoint.java
@@ -0,0 +1,12 @@
+package main;
+
+public class EntryPoint {
+    public static void main(String args[]) {
+        try {
+            MainApp.main(args);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
