cb7949f0750a136ff7a46d7510af018d3bd8e5b4
dnloop <29187104+dnloop@users.noreply.github.com>
2020-05-31 12:21:10 -0300

    Removed old implementation attempt. Set Hibernate initial task.

--- src/main/java/utils/viewswitcher/TaskManager.java
+++ src/main/java/utils/viewswitcher/TaskManager.java
@@ -1,93 +1,62 @@
 package utils.viewswitcher;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+import org.apache.logging.log4j.core.Logger;
 
+import javafx.application.Platform;
 import javafx.concurrent.Task;
-import utils.routes.Route;
-import utils.routes.RouteExtra;
+import utils.DatabaseInitException;
+import utils.DialogBox;
+import utils.HibernateUtil;
+import utils.validator.HibernateValidator;
 
 /**
- * Implementation class for UILoader used on application startup to initialize
- * the visual nodes.
+ * Script containing all available tasks to be loaded in the background.
  * 
  * @author dnloop
  */
-public class InitView {
+public class TaskManager {
 
-    private final List<Task<Void>> taskList = new ArrayList<>();
+    private static final Logger log = (Logger) LogManager.getLogger(TaskManager.class);
 
-    public InitView() {
-    }
+    private static final Marker marker = MarkerManager.getMarker("CLASS");
 
-    public void initTasks() {
-        UILoader uiLoader = new UILoader();
-        // Load main
-        taskList.add(uiLoader.<controller.charts.TotalController>buildTask(RouteExtra.CHART.getPath()));
-        taskList.add(uiLoader.<controller.MainController>buildTask(RouteExtra.MAIN.getPath()));
-        taskList.add(uiLoader.<controller.SelectController>buildTask(RouteExtra.NEW.getPath()));
-        // Load patients
-        taskList.add(uiLoader.<controller.patient.IndexController>buildTask(Route.PACIENTE.indexView()));
-        taskList.add(uiLoader.<controller.patient.ModalDialogController>buildTask(Route.PACIENTE.modalView()));
-        taskList.add(uiLoader.<controller.patient.NewController>buildTask(Route.PACIENTE.newView()));
-        taskList.add(uiLoader.<controller.patient.RecoverController>buildTask(Route.PACIENTE.recoverView()));
-        taskList.add(uiLoader.<controller.patient.MainController>buildTask(RouteExtra.PACIENTEMAIN.getPath()));
-        taskList.add(uiLoader.<controller.patient.ViewController>buildTask(RouteExtra.PACIENTEVIEW.getPath()));
-        // Load clinical files
-        taskList.add(uiLoader.<controller.clinicalFile.IndexController>buildTask(Route.FICHACLINICA.indexView()));
-        taskList.add(uiLoader.<controller.clinicalFile.ModalDialogController>buildTask(Route.FICHACLINICA.modalView()));
-        taskList.add(uiLoader.<controller.clinicalFile.NewController>buildTask(Route.FICHACLINICA.newView()));
-        taskList.add(uiLoader.<controller.clinicalFile.ShowController>buildTask(Route.FICHACLINICA.showView()));
-        taskList.add(uiLoader.<controller.clinicalFile.RecoverController>buildTask(Route.FICHACLINICA.recoverView()));
-        taskList.add(
-                uiLoader.<controller.clinicalFile.OverviewController>buildTask(RouteExtra.CLINICOVERVIEW.getPath()));
-        taskList.add(uiLoader.<controller.clinicalFile.ViewController>buildTask(RouteExtra.CLINICVIEW.getPath()));
-        // Load clinic history
-        taskList.add(uiLoader.<controller.clinicHistory.IndexController>buildTask(Route.HISTORIACLINICA.indexView()));
-        taskList.add(
-                uiLoader.<controller.clinicHistory.ModalDialogController>buildTask(Route.HISTORIACLINICA.modalView()));
-        taskList.add(uiLoader.<controller.clinicHistory.NewController>buildTask(Route.HISTORIACLINICA.newView()));
-        taskList.add(uiLoader.<controller.clinicHistory.ShowController>buildTask(Route.HISTORIACLINICA.showView()));
-        taskList.add(
-                uiLoader.<controller.clinicHistory.RecoverController>buildTask(Route.HISTORIACLINICA.recoverView()));
-        // Load current account
-        taskList.add(uiLoader.<controller.currentAccount.IndexController>buildTask(Route.CUENTACORRIENTE.indexView()));
-        taskList.add(
-                uiLoader.<controller.currentAccount.ModalDialogController>buildTask(Route.CUENTACORRIENTE.modalView()));
-        taskList.add(uiLoader.<controller.currentAccount.NewController>buildTask(Route.CUENTACORRIENTE.newView()));
-        taskList.add(uiLoader.<controller.currentAccount.ShowController>buildTask(Route.CUENTACORRIENTE.showView()));
-        taskList.add(
-                uiLoader.<controller.currentAccount.RecoverController>buildTask(Route.CUENTACORRIENTE.recoverView()));
-        // Load deworming
-        taskList.add(uiLoader.<controller.deworming.IndexController>buildTask(Route.DESPARASITACION.indexView()));
-        taskList.add(uiLoader.<controller.deworming.ModalDialogController>buildTask(Route.DESPARASITACION.modalView()));
-        taskList.add(uiLoader.<controller.deworming.NewController>buildTask(Route.DESPARASITACION.newView()));
-        taskList.add(uiLoader.<controller.deworming.ShowController>buildTask(Route.DESPARASITACION.showView()));
-        taskList.add(uiLoader.<controller.deworming.RecoverController>buildTask(Route.DESPARASITACION.recoverView()));
-        // Load exam
-        taskList.add(uiLoader.<controller.exam.IndexController>buildTask(Route.EXAMEN.indexView()));
-        taskList.add(uiLoader.<controller.exam.ModalDialogController>buildTask(Route.EXAMEN.modalView()));
-        taskList.add(uiLoader.<controller.exam.NewController>buildTask(Route.EXAMEN.newView()));
-        taskList.add(uiLoader.<controller.exam.ShowController>buildTask(Route.EXAMEN.showView()));
-        taskList.add(uiLoader.<controller.exam.RecoverController>buildTask(Route.EXAMEN.recoverView()));
-        taskList.add(uiLoader.<controller.exam.ViewController>buildTask(RouteExtra.EXAMVIEW.getPath()));
-        // Load hospitalization
-        taskList.add(uiLoader.<controller.hospitalization.IndexController>buildTask(Route.INTERNACION.indexView()));
-        taskList.add(
-                uiLoader.<controller.hospitalization.ModalDialogController>buildTask(Route.INTERNACION.modalView()));
-        taskList.add(uiLoader.<controller.hospitalization.NewController>buildTask(Route.INTERNACION.newView()));
-        taskList.add(uiLoader.<controller.hospitalization.ShowController>buildTask(Route.INTERNACION.showView()));
-        taskList.add(uiLoader.<controller.hospitalization.RecoverController>buildTask(Route.INTERNACION.recoverView()));
-        // Load owner
-        taskList.add(uiLoader.<controller.owner.IndexController>buildTask(Route.PROPIETARIO.indexView()));
-        taskList.add(uiLoader.<controller.owner.ModalDialogController>buildTask(Route.PROPIETARIO.modalView()));
-        taskList.add(uiLoader.<controller.owner.NewController>buildTask(Route.PROPIETARIO.newView()));
-        taskList.add(uiLoader.<controller.owner.RecoverController>buildTask(Route.PROPIETARIO.recoverView()));
-        // Load vaccine
-        taskList.add(uiLoader.<controller.vaccine.IndexController>buildTask(Route.VACUNA.indexView()));
-        taskList.add(uiLoader.<controller.vaccine.ModalDialogController>buildTask(Route.VACUNA.modalView()));
-        taskList.add(uiLoader.<controller.vaccine.NewController>buildTask(Route.VACUNA.newView()));
-        taskList.add(uiLoader.<controller.vaccine.ShowController>buildTask(Route.VACUNA.showView()));
-        taskList.add(uiLoader.<controller.vaccine.RecoverController>buildTask(Route.VACUNA.recoverView()));
-    }
+    /**
+     * Sets the initial configuration for Hibernate.
+     * 
+     * @return The Hibernate task.
+     */
+    public static final Task<Void> hibernateConfiguration = new Task<Void>() {
+        @Override
+        protected Void call() throws Exception {
+            log.info(marker, "Loading Database");
+            try {
+                HibernateUtil.setUp();
+                HibernateValidator.buildValid();
+            } catch (DatabaseInitException e) {
+                log.error(marker, "Unable establish the session. " + e.getMessage());
+                e.printStackTrace();
+                DialogBox.setHeader("Error al iniciar la aplicación.");
+                DialogBox.setContent(e.getMessage());
+                DialogBox.displayError();
+                Platform.exit();
+            }
+            return null;
+        }
+
+        @Override
+        protected void cancelled() {
+            updateMessage("Carga cancelada.");
+            log.debug(marker, "Canceled Hibernate module loading.");
+        }
+
+        @Override
+        protected void failed() {
+            updateMessage("Carga fallida.");
+            log.error(marker, "Hibernate module loading failed.");
+        }
+
+    };
 }
