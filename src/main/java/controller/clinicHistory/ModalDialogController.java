badac0181d9cce087fdb628dd33e8c58b86ca496
dnloop <29187104+dnloop@users.noreply.github.com>
2020-06-22 15:23:17 -0300

    fixed null pointer.

--- src/main/java/controller/clinicHistory/ModalDialogController.java
+++ src/main/java/controller/clinicHistory/ModalDialogController.java
@@ -31,144 +31,146 @@ import utils.viewswitcher.ViewSwitcher;
 
 public class ModalDialogController {
 
-    @FXML
-    private JFXButton btnCancel;
+	@FXML
+	private JFXButton btnCancel;
 
-    @FXML
-    private JFXButton btnAccept;
+	@FXML
+	private JFXButton btnAccept;
 
-    @FXML
-    private JFXComboBox<FichasClinicas> comboFC;
+	@FXML
+	private JFXComboBox<FichasClinicas> comboFC;
 
-    @FXML
-    private JFXTextArea txtDescEvento;
+	@FXML
+	private JFXTextArea txtDescEvento;
 
-    @FXML
-    private JFXTextField txtResultado;
+	@FXML
+	private JFXTextField txtResultado;
 
-    @FXML
-    private JFXTextField txtSecuelas;
+	@FXML
+	private JFXTextField txtSecuelas;
 
-    @FXML
-    private JFXTextField txtConsideraciones;
+	@FXML
+	private JFXTextField txtConsideraciones;
 
-    @FXML
-    private JFXTextArea txtComentarios;
+	@FXML
+	private JFXTextArea txtComentarios;
 
-    @FXML
-    private DatePicker dpFechaInicio;
+	@FXML
+	private DatePicker dpFechaInicio;
 
-    @FXML
-    private DatePicker dpFechaResolucion;
+	@FXML
+	private DatePicker dpFechaResolucion;
 
-    private static final Logger log = (Logger) LogManager.getLogger(ModalDialogController.class);
+	private static final Logger log = (Logger) LogManager.getLogger(ModalDialogController.class);
 
-    private static final Marker marker = MarkerManager.getMarker("CLASS");
+	private static final Marker marker = MarkerManager.getMarker("CLASS");
 
-    private FichasClinicasHome daoFC = new FichasClinicasHome();
+	private FichasClinicasHome daoFC = new FichasClinicasHome();
 
-    private HistoriaClinicaHome daoCH = new HistoriaClinicaHome();
+	private HistoriaClinicaHome daoCH = new HistoriaClinicaHome();
 
-    private HistoriaClinica historiaClinica;
+	private HistoriaClinica historiaClinica;
 
-    final ObservableList<FichasClinicas> fichasList = FXCollections.observableArrayList();
+	final ObservableList<FichasClinicas> fichasList = FXCollections.observableArrayList();
 
-    private Date fechaResolucion;
+	private Date fechaResolucion;
 
-    private LocalDate lfechaResolucion;
+	private LocalDate lfechaResolucion;
 
-    private Date fechaInicio;
+	private Date fechaInicio;
 
-    private LocalDate lfechaInicio;
+	private LocalDate lfechaInicio;
 
-    @FXML
-    void initialize() {
+	@FXML
+	void initialize() {
 
-        Platform.runLater(() -> loadingFields());
+		Platform.runLater(() -> loadingFields());
 
-        loadDao();
+		loadDao();
 
-        btnCancel.setOnAction((event) -> {
-            ViewSwitcher.modalStage.close();
-        });
+		btnCancel.setOnAction((event) -> {
+			ViewSwitcher.modalStage.close();
+		});
 
-        btnAccept.setOnAction((event) -> {
-            if (DialogBox.confirmDialog("¿Desea actualizar el registro?"))
-                updateRecord();
-        });
+		btnAccept.setOnAction((event) -> {
+			if (DialogBox.confirmDialog("¿Desea actualizar el registro?"))
+				updateRecord();
+		});
 
-    }
+	}
 
-    /*
-     * Class Methods
-     */
+	/*
+	 * Class Methods
+	 */
 
-    /**
-     * Extracts the fields to create a clinic history object used to update the
-     * database.
-     */
-    private void updateRecord() {
-        // date conversion from LocalDate
-        fechaResolucion = java.sql.Date.valueOf(dpFechaResolucion.getValue());
-        fechaInicio = java.sql.Date.valueOf(dpFechaInicio.getValue());
-        fechaResolucion = new Date(); // recycling
-        historiaClinica.setFechaResolucion(fechaResolucion);
-        historiaClinica.setFechaInicio(fechaInicio);
-        historiaClinica.setResultado(txtResultado.getText());
-        historiaClinica.setSecuelas(txtSecuelas.getText());
-        historiaClinica.setConsideraciones(txtConsideraciones.getText());
-        historiaClinica.setComentarios(txtComentarios.getText());
-        historiaClinica.setDescripcionEvento(txtDescEvento.getText());
-        historiaClinica.setFichasClinicas(comboFC.getSelectionModel().getSelectedItem());
-        historiaClinica.setUpdatedAt(fechaResolucion);
-        if (HibernateValidator.validate(historiaClinica)) {
-            daoCH.update(historiaClinica);
-            log.info("Record updated.");
-            DialogBox.displaySuccess();
-            ViewSwitcher.modalStage.close();
-        } else {
-            DialogBox.setHeader("Fallo en la carga del registro");
-            DialogBox.setContent(HibernateValidator.getError());
-            DialogBox.displayError();
-            HibernateValidator.resetError();
-            log.error(marker, "Failed to update record.");
-        }
-    }
+	/**
+	 * Extracts the fields to create a clinic history object used to update the
+	 * database.
+	 */
+	private void updateRecord() {
+		// date conversion from LocalDate
+		fechaResolucion = java.sql.Date.valueOf(dpFechaResolucion.getValue());
+		fechaInicio = java.sql.Date.valueOf(dpFechaInicio.getValue());
+		fechaResolucion = new Date(); // recycling
+		historiaClinica.setFechaResolucion(fechaResolucion);
+		historiaClinica.setFechaInicio(fechaInicio);
+		historiaClinica.setResultado(txtResultado.getText());
+		historiaClinica.setSecuelas(txtSecuelas.getText());
+		historiaClinica.setConsideraciones(txtConsideraciones.getText());
+		historiaClinica.setComentarios(txtComentarios.getText());
+		historiaClinica.setDescripcionEvento(txtDescEvento.getText());
+		historiaClinica.setFichasClinicas(comboFC.getSelectionModel().getSelectedItem());
+		historiaClinica.setUpdatedAt(fechaResolucion);
+		if (HibernateValidator.validate(historiaClinica)) {
+			daoCH.update(historiaClinica);
+			log.info("Record updated.");
+			DialogBox.displaySuccess();
+			ViewSwitcher.modalStage.close();
+		} else {
+			DialogBox.setHeader("Fallo en la carga del registro");
+			DialogBox.setContent(HibernateValidator.getError());
+			DialogBox.displayError();
+			HibernateValidator.resetError();
+			log.error(marker, "Failed to update record.");
+		}
+	}
 
-    public void setObject(HistoriaClinica historiaClinica) {
-        this.historiaClinica = historiaClinica;
-    }
+	public void setObject(HistoriaClinica historiaClinica) {
+		this.historiaClinica = historiaClinica;
+	}
 
-    public void loadDao() {
-        Task<List<FichasClinicas>> task = daoFC.displayRecords();
+	public void loadDao() {
+		Task<List<FichasClinicas>> task = daoFC.displayRecords();
 
-        task.setOnSucceeded(event -> {
-            fichasList.setAll(task.getValue());
-            comboFC.setItems(fichasList);
-            for (FichasClinicas ficha : comboFC.getItems())
-                if (historiaClinica.getFichasClinicas().getPacientes().getId().equals(ficha.getPacientes().getId())) {
-                    comboFC.getSelectionModel().select(ficha);
-                    break;
-                }
-            log.info(marker, "List Loaded.");
-        });
+		task.setOnSucceeded(event -> {
+			fichasList.setAll(task.getValue());
+			comboFC.setItems(fichasList);
+			for (FichasClinicas ficha : comboFC.getItems())
+				if (historiaClinica.getFichasClinicas().getPacientes().getId().equals(ficha.getPacientes().getId())) {
+					comboFC.getSelectionModel().select(ficha);
+					break;
+				}
+			log.info(marker, "List Loaded.");
+		});
 
-        ViewSwitcher.loadingDialog.addTask(task);
-        ViewSwitcher.loadingDialog.startTask();
-    }
+		ViewSwitcher.loadingDialog.addTask(task);
+		ViewSwitcher.loadingDialog.startTask();
+	}
 
-    private void loadingFields() {
-        log.info(marker, "Loading fields");
-        fechaResolucion = new Date(historiaClinica.getFechaResolucion().getTime());
-        lfechaResolucion = fechaResolucion.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        fechaInicio = new Date(historiaClinica.getFechaInicio().getTime());
-        lfechaInicio = fechaInicio.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        dpFechaResolucion.setValue(lfechaResolucion);
-        dpFechaInicio.setValue(lfechaInicio);
-        txtResultado.setText(historiaClinica.getResultado());
-        txtSecuelas.setText(historiaClinica.getSecuelas());
-        txtConsideraciones.setText(historiaClinica.getConsideraciones());
-        txtComentarios.setText(historiaClinica.getComentarios());
-        txtDescEvento.setText(historiaClinica.getDescripcionEvento());
-    }
+	private void loadingFields() {
+		log.info(marker, "Loading fields");
+		if (historiaClinica.getFechaResolucion() != null) {
+			fechaResolucion = new Date(historiaClinica.getFechaResolucion().getTime());
+			lfechaResolucion = fechaResolucion.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		}
+		fechaInicio = new Date(historiaClinica.getFechaInicio().getTime());
+		lfechaInicio = fechaInicio.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		dpFechaResolucion.setValue(lfechaResolucion);
+		dpFechaInicio.setValue(lfechaInicio);
+		txtResultado.setText(historiaClinica.getResultado());
+		txtSecuelas.setText(historiaClinica.getSecuelas());
+		txtConsideraciones.setText(historiaClinica.getConsideraciones());
+		txtComentarios.setText(historiaClinica.getComentarios());
+		txtDescEvento.setText(historiaClinica.getDescripcionEvento());
+	}
 }
--- src/main/java/model/HistoriaClinica.java
+++ src/main/java/model/HistoriaClinica.java
@@ -12,172 +12,171 @@ import javax.validation.constraints.Size;
  */
 public class HistoriaClinica extends BaseModel {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = -2495196227762558509L;
-    private Integer id;
-    @NotNull(message = "El campo 'paciente (Ficha)' es requerido.")
-    private FichasClinicas fichasClinicas;
-    @NotEmpty(message = "El campo 'Descripción' es requerido.")
-    @Size(max = 65535, message = "El campo 'Descripción' no debe ser mayor a {max} caracteres.")
-    private String descripcionEvento;
-    @NotNull(message = "El campo 'Fecha Inicio' es requerido.")
-    private Date fechaInicio;
-    private Date fechaResolucion;
-    @Size(max = 500, message = "El campo 'resultado' no debe ser mayor a {max} caracteres.")
-    private String resultado;
-    @Size(max = 500, message = "El campo 'secuela' no debe ser mayor a {max} caracteres.")
-    private String secuelas;
-    @Size(max = 500, message = "El campo 'consideraciones' no debe ser mayor a {max} caracteres.")
-    private String consideraciones;
-    @Size(max = 65535, message = "El campo 'comentarios' no debe ser mayor a {max} caracteres.")
-    private String comentarios;
-    private boolean deleted;
-    private Date createdAt;
-    private Date updatedAt;
-    private Date deletedAt;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -2495196227762558509L;
+	private Integer id;
+	@NotNull(message = "El campo 'paciente (Ficha)' es requerido.")
+	private FichasClinicas fichasClinicas;
+	@NotEmpty(message = "El campo 'Descripción' es requerido.")
+	@Size(max = 65535, message = "El campo 'Descripción' no debe ser mayor a {max} caracteres.")
+	private String descripcionEvento;
+	@NotNull(message = "El campo 'Fecha Inicio' es requerido.")
+	private Date fechaInicio;
+	private Date fechaResolucion;
+	@Size(max = 500, message = "El campo 'resultado' no debe ser mayor a {max} caracteres.")
+	private String resultado;
+	@Size(max = 500, message = "El campo 'secuela' no debe ser mayor a {max} caracteres.")
+	private String secuelas;
+	@Size(max = 500, message = "El campo 'consideraciones' no debe ser mayor a {max} caracteres.")
+	private String consideraciones;
+	@Size(max = 65535, message = "El campo 'comentarios' no debe ser mayor a {max} caracteres.")
+	private String comentarios;
+	private boolean deleted;
+	private Date createdAt;
+	private Date updatedAt;
+	private Date deletedAt;
 
-    public HistoriaClinica() {
-    }
+	public HistoriaClinica() {
+	}
 
-    public HistoriaClinica(FichasClinicas fichasClinicas, String descripcionEvento, Date fechaInicio, boolean deleted) {
-        this.fichasClinicas = fichasClinicas;
-        this.descripcionEvento = descripcionEvento;
-        this.deleted = deleted;
-    }
+	public HistoriaClinica(FichasClinicas fichasClinicas, String descripcionEvento, Date fechaInicio, boolean deleted) {
+		this.fechaInicio = fechaInicio;
+		this.fichasClinicas = fichasClinicas;
+		this.descripcionEvento = descripcionEvento;
+		this.deleted = deleted;
+	}
 
-    public HistoriaClinica(
-            FichasClinicas fichasClinicas, String descripcionEvento, Date fechaInicio, Date fechaResolucion,
-            String resultado, String secuelas, String consideraciones, String comentarios, boolean deleted,
-            Date createdAt, Date updatedAt, Date deletedAt
-    ) {
-        super();
-        this.fichasClinicas = fichasClinicas;
-        this.descripcionEvento = descripcionEvento;
-        this.fechaInicio = fechaInicio;
-        this.fechaResolucion = fechaResolucion;
-        this.resultado = resultado;
-        this.secuelas = secuelas;
-        this.consideraciones = consideraciones;
-        this.comentarios = comentarios;
-        this.deleted = deleted;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
-    }
+	public HistoriaClinica(FichasClinicas fichasClinicas, String descripcionEvento, Date fechaInicio,
+			Date fechaResolucion, String resultado, String secuelas, String consideraciones, String comentarios,
+			boolean deleted, Date createdAt, Date updatedAt, Date deletedAt) {
+		super();
+		this.fichasClinicas = fichasClinicas;
+		this.descripcionEvento = descripcionEvento;
+		this.fechaInicio = fechaInicio;
+		this.fechaResolucion = fechaResolucion;
+		this.resultado = resultado;
+		this.secuelas = secuelas;
+		this.consideraciones = consideraciones;
+		this.comentarios = comentarios;
+		this.deleted = deleted;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.deletedAt = deletedAt;
+	}
 
-    @Override
-    public Integer getId() {
-        return this.id;
-    }
+	@Override
+	public Integer getId() {
+		return this.id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public FichasClinicas getFichasClinicas() {
-        return this.fichasClinicas;
-    }
+	public FichasClinicas getFichasClinicas() {
+		return this.fichasClinicas;
+	}
 
-    public void setFichasClinicas(FichasClinicas fichasClinicas) {
-        this.fichasClinicas = fichasClinicas;
-    }
+	public void setFichasClinicas(FichasClinicas fichasClinicas) {
+		this.fichasClinicas = fichasClinicas;
+	}
 
-    public String getDescripcionEvento() {
-        return this.descripcionEvento;
-    }
+	public String getDescripcionEvento() {
+		return this.descripcionEvento;
+	}
 
-    public void setDescripcionEvento(String descripcionEvento) {
-        this.descripcionEvento = descripcionEvento;
-    }
+	public void setDescripcionEvento(String descripcionEvento) {
+		this.descripcionEvento = descripcionEvento;
+	}
 
-    public Date getFechaResolucion() {
-        return this.fechaResolucion;
-    }
+	public Date getFechaResolucion() {
+		return this.fechaResolucion;
+	}
 
-    public void setFechaResolucion(Date fechaResolucion) {
-        this.fechaResolucion = fechaResolucion;
-    }
+	public void setFechaResolucion(Date fechaResolucion) {
+		this.fechaResolucion = fechaResolucion;
+	}
 
-    public String getResultado() {
-        return this.resultado;
-    }
+	public String getResultado() {
+		return this.resultado;
+	}
 
-    public void setResultado(String resultado) {
-        this.resultado = resultado;
-    }
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
 
-    public String getSecuelas() {
-        return this.secuelas;
-    }
+	public String getSecuelas() {
+		return this.secuelas;
+	}
 
-    public void setSecuelas(String secuelas) {
-        this.secuelas = secuelas;
-    }
+	public void setSecuelas(String secuelas) {
+		this.secuelas = secuelas;
+	}
 
-    public String getConsideraciones() {
-        return this.consideraciones;
-    }
+	public String getConsideraciones() {
+		return this.consideraciones;
+	}
 
-    public void setConsideraciones(String consideraciones) {
-        this.consideraciones = consideraciones;
-    }
+	public void setConsideraciones(String consideraciones) {
+		this.consideraciones = consideraciones;
+	}
 
-    public String getComentarios() {
-        return this.comentarios;
-    }
+	public String getComentarios() {
+		return this.comentarios;
+	}
 
-    public void setComentarios(String comentarios) {
-        this.comentarios = comentarios;
-    }
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
 
-    @Override
-    public Date getCreatedAt() {
-        return this.createdAt;
-    }
+	@Override
+	public Date getCreatedAt() {
+		return this.createdAt;
+	}
 
-    @Override
-    public boolean isDeleted() {
-        return this.deleted;
-    }
+	@Override
+	public boolean isDeleted() {
+		return this.deleted;
+	}
 
-    @Override
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
+	@Override
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
 
-    @Override
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+	@Override
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
 
-    @Override
-    public Date getUpdatedAt() {
-        return this.updatedAt;
-    }
+	@Override
+	public Date getUpdatedAt() {
+		return this.updatedAt;
+	}
 
-    @Override
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+	@Override
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 
-    @Override
-    public Date getDeletedAt() {
-        return this.deletedAt;
-    }
+	@Override
+	public Date getDeletedAt() {
+		return this.deletedAt;
+	}
 
-    @Override
-    public void setDeletedAt(Date deletedAt) {
-        this.deletedAt = deletedAt;
-    }
+	@Override
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
 
-    public Date getFechaInicio() {
-        return fechaInicio;
-    }
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
 
-    public void setFechaInicio(Date fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
 
 }
